44ac5fd210922e8f6b21c876bcaa60047d283df9
henriqueCostaSI <henrique.mcosta@hotmail.com>
2022-10-04 22:33:40 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/* Making a request to the API. 
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+.then( response => response.json())
+.then( r => { 
+    if (r.erro){
+        throw Error('Esse Cep Não Existe')
+    } else
+
+    console.log(r)
+})
+.catch( error => console.log(error))
+.finally(mensagemos => console.log('Processamento concluído!'));
+
+console.log(consultaCep);
+
+*/
+
+async function buscaEndereco(cep) {
+    try {
+        var consultaCep = await fetch(fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if(consultaCepConvertida.erro) {
+            throw Error('Cep não existe!')
+        }
+
+        console.log(consultaCepConvertida);
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+buscaEndereco();
